8e733285eadd6fb3986422ac2683307a2924e35b
Vivian Menard <vivian.menard@fantasiapp.com>
2021-08-25 12:05:14 +0200

    Clean version of getData request

--- visioServer/dataStructure.py
+++ visioServer/dataStructure.py
@@ -1,10 +1,8 @@
-import time
-
-def buildStructure(name:str, steps:list, pdvs:list):
+def buildTree(name, steps:list, pdvs:list):
     if not steps:
         return [name, list(pdvs.keys())]
     nextKey = steps[0]
-    children = [buildStructure(name, steps[1:], associatedPdvs) for name, associatedPdvs in diceByKey(nextKey, pdvs).items()]
+    children = [buildTree(name, steps[1:], associatedPdvs) for name, associatedPdvs in diceByKey(nextKey, pdvs).items()]
     return [name, children]
 
 def diceByKey(index, pdvs:list):
@@ -15,16 +13,12 @@ def diceByKey(index, pdvs:list):
         dicedBykey[pdv[index]][id] = pdv
     return dicedBykey 
 
-def diceSales(sales:list):
+def formatSales(sales:list):
+    print('Formating sales...')
     dicedSales = {}
-    start = time.time()
     for sale in sales:
-        id = sale.pdv.id
+        id = str(sale.pdv.id)
         if id not in dicedSales:
             dicedSales[id] = []
-        dicedSales[id].append([sale.industry.id, sale.product.id, sale.volume]) 
-    print(f'duration: {time.time() - start}')
+        dicedSales[id].append([sale.industry.id, sale.product.id, sale.volume])
     return dicedSales
-
-def formatPdv(pdvs:list, associatedSales:dict):
-    return {pdv['id']:[value for key, value in pdv.items() if key != 'id'] + associatedSales.get(pdv['id'], [[]]) for pdv in pdvs}
--- visioServer/views.py
+++ visioServer/views.py
@@ -1,29 +1,35 @@
-from django.http.response import JsonResponse
-# from django.shortcuts import render
-from django.contrib import auth
 from .models import *
-#import django.apps
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from .dataStructure import formatPdv, buildStructure, diceSales
-import time
+from .dataStructure import buildTree, formatSales
+import json
 
 class DefaultView(APIView):
     permission_classes = (IsAuthenticated,)
 class Data(DefaultView):
-    salesDict = diceSales(Ventes.objects.all())
+    config = None
+    with open('visioServer/config.json', 'r') as cfgFile:
+        config = json.load(cfgFile)
+    salesDict = None
+    try:
+        with open('visioServer/salesDict.json', 'r') as jsonFile:
+            salesDict = json.load(jsonFile)
+    except:
+        salesDict = formatSales(Ventes.objects.all())
+        with open('visioServer/salesDict.json', 'w') as jsonFile:
+            json.dump(salesDict, jsonFile)
 
     def get(self, request):
         data = {}
-        data['Pdv'] = formatPdv([model_to_dict(object) for object in Pdv.objects.all()], self.salesDict)
-        # A terme il faudra faire quelque chose de plus générique avec django.apps.apps.get_models() pour récup les models
-        # Ou alors on peut juste stocker cette liste dans une config
-        for model in [Drv, Agent, AgentFinitions, Dep, Bassin, Ville, SegmentMarketing, SegmentCommercial, Enseigne, Ensemble, SousEnsemble, Site, Produit, Industrie]:
+        data['Pdv'] = self.__formatPdv([model_to_dict(object) for object in Pdv.objects.all()])
+        regularModels = [eval(modelName) for modelName in self.config["regularModels"]]
+        for model in regularModels:
             data.update({model.__name__: {object.id: object.name for object in model.objects.all()}})
-        geoStructure = [2,3,4,5] # Correspond à Drv, Agent, Département, Bassin
-        brandStructure = [11, 12, 13] # Correspond à Enseigne, Ensemble, Sous-Ensemble
-        data['geoTree'] = buildStructure('root', geoStructure, data['Pdv'])
-        data['brandTree'] = buildStructure('root', brandStructure, data['Pdv'])
+        data['geoTree'] = buildTree('root', self.config["geoTreeStructure"], data['Pdv'])
+        data['brandTree'] = buildTree('root', self.config["brandTreeStructure"], data['Pdv'])
         return Response(data)
+    
+    def __formatPdv(self, pdvs:list):
+        return {pdv['id']:[value for key, value in pdv.items() if key != 'id'] + [self.salesDict.get(str(pdv['id']), [])] for pdv in pdvs}
