29fdaffa76f6c1b0c8cadda7a3a233fb6fdd34e3
Vivian Menard <vivian.menard@fantasiapp.com>
2021-08-25 18:31:57 +0200

    little modifications on getData request

--- visioServer/views.py
+++ visioServer/views.py
@@ -1,3 +1,4 @@
+from django.db.models import indexes
 from .models import *
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
@@ -5,11 +6,13 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .dataStructure import buildTree, formatSales
 import json
+from django.db.models.fields.related import ForeignKey
 
 class DefaultView(APIView):
     permission_classes = (IsAuthenticated,)
 class Data(DefaultView):
     config = None
+    # Il faut faire en sorte que ce tricks ne soit fait qu'en local et pas sur le server, on peut utilser qqchose du genre "if debug :"
     with open('visioServer/config.json', 'r') as cfgFile:
         config = json.load(cfgFile)
     salesDict = None
@@ -23,13 +26,22 @@ class Data(DefaultView):
 
     def get(self, request):
         data = {}
-        data['Pdv'] = self.__formatPdv([model_to_dict(object) for object in Pdv.objects.all()])
+        formatedPdvs, data['Pdv'] = self.__formatPdv([model_to_dict(object) for object in Pdv.objects.all()])
         regularModels = [eval(modelName) for modelName in self.config["regularModels"]]
         for model in regularModels:
-            data.update({model.__name__: {object.id: object.name for object in model.objects.all()}})
-        data['geoTree'] = buildTree('root', self.config["geoTreeStructure"], data['Pdv'])
-        data['brandTree'] = buildTree('root', self.config["brandTreeStructure"], data['Pdv'])
+            data.update({model.__name__.lower(): {object.id: object.name for object in model.objects.all()}})
+        data['geoTree'] = buildTree('root', self.config["geoTreeStructure"], formatedPdvs)
+        data['brandTree'] = buildTree('root', self.config["brandTreeStructure"], formatedPdvs)
         return Response(data)
     
     def __formatPdv(self, pdvs:list):
-        return {pdv['id']:[value for key, value in pdv.items() if key != 'id'] + [self.salesDict.get(str(pdv['id']), [])] for pdv in pdvs}
+        formatedPdvs = {pdv['id']:[value for key, value in pdv.items() if key != 'id'] + [self.salesDict.get(str(pdv['id']), [])] for pdv in pdvs}
+        fields = list(pdvs[0].keys())[1:]
+        indexes = []
+        for fieldName in fields:
+            if type(Pdv._meta.get_field(fieldName)) is ForeignKey:
+                indexes.append(fields.index(fieldName))
+        fields += ['sales']
+        dataPdv = {'fields' : fields, 'indexes': indexes}
+        dataPdv.update(formatedPdvs)
+        return formatedPdvs, dataPdv
