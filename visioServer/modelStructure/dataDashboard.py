bfddbe2bfdf35fc66a5f456977a5f1d3e5fa1eba
jeanluc <jeanluc.walter@fantasiapp.com>
2021-09-08 15:45:50 +0200

    dataDashoard.py print for test

--- visioServer/modelStructure/dataDashboard.py
+++ visioServer/modelStructure/dataDashboard.py
@@ -121,6 +121,7 @@ class Navigation(DataGeneric):
 
   def __createModels(self, data):
     models = self.config["navModels"]
+    print("__createModels", models, self.__userGroup)
     if self.__userGroup != "root":
       del models[0]
     if self.__userGroup == "agent":
