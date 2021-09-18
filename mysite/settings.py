d009375d5f4161c614755891231ea59345cf0f46
jeanluc <jeanluc.walter@fantasiapp.com>
2021-09-18 10:39:44 +0200

    commit reverse setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = [os.getenv('URL'), '127.0.0.1', '.pythonanywhere.com']
 print("ALLOWED_HOSTS", ALLOWED_HOSTS)
 
 
@@ -47,14 +47,9 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'sslserver',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
