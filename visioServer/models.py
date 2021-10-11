6c0f8a151f606d1c8de55a118a27dc6f0aa26c7a
jeanluc <jeanluc.walter@fantasiapp.com>
2021-10-11 13:35:59 +0200

    update target test 8

--- visioServer/models.py
+++ visioServer/models.py
@@ -78,7 +78,7 @@ class CommonModel(models.Model):
 
   @classmethod
   def getDataFromDict(cls, field, data):
-    structureData = cls.ListFields
+    structureData = cls.ListFields()
     try:
       indexField = structureData.index(field)
     except ValueError:
@@ -544,7 +544,7 @@ class Ciblage(CommonModel):
     return lf
 
   def update(self, data, now):
-    print("update", data, self.ListFields, now)
+    print("update", data, self.ListFields(), now)
     print("update", self.getDataFromDict("redistributed", data))
 
 class CiblageLevel(models.Model):
