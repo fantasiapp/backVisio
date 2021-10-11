650f4539fef59e76c1faa6bd761861bff8584ee1
jeanluc <jeanluc.walter@fantasiapp.com>
2021-10-11 13:38:22 +0200

    update target test 9

--- visioServer/modelStructure/dataDashboard.py
+++ visioServer/modelStructure/dataDashboard.py
@@ -258,7 +258,7 @@ class DataDashboard:
     targetObject = Ciblage.objects.filter(pdv=pdv)
     if targetObject:
       print("__updateDataBaseTarget", target, targetObject)
-      targetObject[0].update(target, getattr(self, "__structurePdvs"), now)
+      targetObject[0].update(target, target, now)
     else:
       print("need to Create Target Object")
     print("end")
