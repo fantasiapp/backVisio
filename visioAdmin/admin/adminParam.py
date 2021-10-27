361057a008026bfcc8a02ff8778cee093bbac1bf
jeanluc <jeanluc.walter@fantasiapp.com>
2021-10-27 11:08:30 +0200

    activate adminParam 2

--- visioAdmin/admin/adminParam.py
+++ visioAdmin/admin/adminParam.py
@@ -13,7 +13,7 @@ class AdminParam:
     ParamVisio.setValue("isAdOpen", False if isAdOpen else True)
     if ParamVisio.getValue("isAdOpen"):
       print("clean date")
-      for sale in Sales.objects.filter(dater__isnull=False):
+      for sale in Sales.objects.filter(date__isnull=False):
         sale.date = None
         sale.save()
     after = "Fermée" if isAdOpen else "Ouverte"
