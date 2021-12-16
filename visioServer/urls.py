ddbfb02515e02bdc0320722e29e9c10e8ff03c6a
jeanluc <jeanluc.walter@fantasiapp.com>
2021-12-16 16:34:30 +0100

    visioAdmin check login 4

--- visioServer/urls.py
+++ visioServer/urls.py
@@ -4,6 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
   # url(r'^login', views.login, name='login'),
-  url('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-  url('data/', views.Data.as_view(), name='data'),
+  re_path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+  re_path('data/', views.Data.as_view(), name='data'),
 ]
