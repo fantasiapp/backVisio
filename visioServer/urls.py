9a1fb0bec99e88b224482ee4c6d05bde6605938a
jean-luc walter <jeanluc.walter@fantasiapp.com>
2022-05-04 10:54:10 +0200

    start Oauth2 1

--- visioServer/urls.py
+++ visioServer/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   # url(r'^login', views.login, name='login'),
   re_path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
   re_path('data/', views.Data.as_view(), name='data'),
+  re_path('api-token-auth-google/', views.Data.as_view(), name='data'),
 ]
